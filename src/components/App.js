05fd62507df72f589b660e8baa3197b3eebc0a2d
Vinh Dang <vinhdang@Vinhs-MacBook-Pro.local>
2018-08-08 07:13:43 -0700

    Initializing project

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <div className="catch-of-the-day">
+          <div className="menu">hello</div>
+          <div className="order-wrap">hi</div>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
